a278191d078444ef9dafad1b0a84664046bfb08b
LEE JAE UK <sjrnfl1746@gmail.com>
2025-10-09 17:45:08 +0900

    ..

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -7,13 +7,13 @@ const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
 
       SaladayDesc:"Spring Boot, REST API, React로 샐러드 판매 키오스크 및 Admin 페이지를 구현해 보았습니다.",
-      SaladayGithub:"https://github.com/sjrnfl1746",
+      SaladayGithub:"https://github.com/sjrnfl1746/saladay_admin",
 
       DiaryDesc:"React를 활용하여 감정 일기장을 구현해 보았습니다.",
       DiaryGithub:"https://github.com/sjrnfl1746/react_edu_diary_250805",
 
       MyPetLogDesc:"React, Spring Boot Api를 활용하여 반려동물 커뮤니티 및 쇼핑몰 사이트를 구현해 보았습니다.",
-      MyPetLogGithub:"https://github.com/sjrnfl1746/",
+      MyPetLogGithub:"https://github.com/sjrnfl1746/mypetlog_customer",
   }
 
   let show ='';
